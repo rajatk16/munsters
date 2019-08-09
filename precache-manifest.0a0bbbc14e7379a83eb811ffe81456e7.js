86184b0fad655accd012e175584058cff35dfad0
Rajat Sudagade <rajat.k16@gmail.com>
2019-08-08 21:20:16 -0500

    Updates

---
+++ precache-manifest.0a0bbbc14e7379a83eb811ffe81456e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcbb3540a8ef0c9808e729b4d3e8f996",
+    "url": "/munsters/index.html"
+  },
+  {
+    "revision": "518db87fdfaad479c37a",
+    "url": "/munsters/static/css/main.8242db10.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/munsters/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "518db87fdfaad479c37a",
+    "url": "/munsters/static/js/main.2fde7559.chunk.js"
+  },
+  {
+    "revision": "07a89091fe5080d2bc59",
+    "url": "/munsters/static/js/runtime~main.1d03a838.js"
+  }
+]);
